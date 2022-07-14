1323274e42fd8eb65648f50b2a3d97383bae97b9
Luca Benaglio <lucabena1031@gmail.com>
2022-07-14 18:51:56 -0300

    fin del codigo y subida a github

--- codigo.js
+++ codigo.js
@@ -1,3 +1,5 @@
+//creamos nuestra plantilla de humanos
+
 class Humano {
     constructor(nombre = " ", apellido = " ", edad = 1, peso = 1) {
         this.nombre = nombre
@@ -7,14 +9,66 @@ class Humano {
     }
 }
 
+//creamos las 3 funciones que usaremos al final
+
+function buscarNombre(humanos) {
+    let nombreHumano = prompt("Ingrese el nombre de su humano")
+
+    let nombreBuscado = humanos.find(humanosCreados => humanosCreados.nombre == nombreHumano)
+
+    if(nombreBuscado == undefined) {
+        alert("Humano no encontrado")
+    } 
+
+    else {
+        console.log(nombreBuscado)
+    }
+}
+
+function buscarApellido(humanos) {
+    let apellidoHumano = prompt("Ingrese el apellido de su humano")
+
+    let apellidoBuscado = humanos.find(humanosCreados => humanosCreados.apellido == apellidoHumano)
+
+    if(apellidoBuscado == undefined) {
+        alert("Humano no encontrado")
+    } 
+
+    else {
+        console.log(apellidoBuscado)
+    }
+}
+
+function ordenarHumanos(humanos) {
+
+    let metodoOrdenar = parseInt(prompt("Ingrese 1 para ordenar de menor a mayor, 2 para ordenar de mayor a menor"))
+
+    if(metodoOrdenar === 1 ) {
+        console.log(humanos.sort((a,b) => a.edad - b.edad))
+    } 
+    else if (metodoOrdenar === 2) {
+        console.log(humanos.sort((a,b) => b.edad - a.edad))
+    }
+
+}
+
+//creamos un array vacio para que el usuario lo llene con datos
+
 const humanos = []
 
-let nombre, apellido, edad, peso, continua
+//creamos las variables que las rellenaremos despues
+
+let nombre, apellido, edad, peso, continua, respuesta
+
+//creamos ciclos donde el usuario ingresara datos
 
 do {
 
-    do{
-        alert("Bienvenido al cielo, a crear humanos :)")
+    do {
+        alert(`Bienvenido al cielo :)
+        - cree humanos con las caracteristicas solicitadas
+        - cree 2 o mas humanos para poder usar las funciones correctamente
+        `)
 
         nombre = prompt("Ingrese el nombre de su humano").toLowerCase()
         apellido = prompt("Ingrese el apellido de su humano").toLowerCase()
@@ -26,7 +80,34 @@ do {
     } while ((isNaN(edad) || isNaN(peso) || (edad <= 0 || peso <= 0)))
     
     const humanosCreados = new Humano(nombre, apellido, edad, peso)
+    humanos.push(humanosCreados)
 
     continua = prompt(`¿Quiere crear otro humano? "si" o "no"`).toLowerCase()
 
 } while (continua != "no")
+
+console.log(humanos)
+
+//creamos otras interacciones donde implementamos las funciones con los metodos
+
+do {
+    respuesta = parseInt(prompt(`Ingrese 1 para:
+    1- Buscar por nombre
+    2- Buscar por apellido
+    3- Ordenar de menor a mayor (edad)
+    `))
+} while(respuesta <1 || respuesta >3)
+
+switch(respuesta) {
+    case 1:
+        buscarNombre(humanos)
+        break
+    case 2:
+        buscarApellido(humanos)
+        break
+    case 3:
+        ordenarHumanos(humanos)
+        break
+    default:
+        alert("Algo salio mal :(")
+}
